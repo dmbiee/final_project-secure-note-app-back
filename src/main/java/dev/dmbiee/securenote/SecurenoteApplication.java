b2a7e5f8b793432a198b48d2a8ef5dbe77d63f9b
Dmytro Belei <beleyhha@gmail.com>
2025-10-27 15:26:41 +0100

    init: init project

---
+++ src/main/java/dev/dmbiee/securenote/SecurenoteApplication.java
@@ -0,0 +1,13 @@
+package dev.dmbiee.securenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurenoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurenoteApplication.class, args);
+	}
+
+}
