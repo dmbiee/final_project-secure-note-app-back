e8c90981792e78341f10089af791eaf653f1f803
Dmytro Belei <beleyhha@gmail.com>
2025-10-31 18:32:07 +0100

    feat: add isAutorizaded api

--- src/main/java/dev/dmbiee/securenote/config/SecurityConfig.java
+++ src/main/java/dev/dmbiee/securenote/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package dev.dmbiee.securenote.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import dev.dmbiee.securenote.core.security.JwtAuthFilter;
 
@@ -25,23 +30,37 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(
 
                                 "/h2-console/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**/index.html",
-                                "/swagger-ui/**/favicon.ico")
-                        .permitAll()
+                                "/v3/api-docs/**").permitAll()
+
+                                // "/swagger-ui/**/index.html",
+                                // "/swagger-ui/**/favicon.ico"                        .requestMatchers("/api/test/**").authenticated() // тільки для авторизованих
                         .anyRequest().authenticated())
                 // .anyRequest().permitAll())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // твій фронт
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true); // якщо передаєш cookie
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
