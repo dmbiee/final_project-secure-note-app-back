d6f53c93eb3f4c4beb791b93f8de078f12d52809
Dmytro Belei <beleyhha@gmail.com>
2025-11-04 08:52:20 +0100

    feat: add note entity

---
+++ src/main/java/dev/dmbiee/securenote/features/note/Note.java
@@ -0,0 +1,28 @@
+package dev.dmbiee.securenote.features.note;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "notes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private LocalDate date;
+
+    private String owner;
+}
