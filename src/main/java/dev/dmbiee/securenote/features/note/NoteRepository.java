eff7de6ebaf7a2d1a8edc8d018c821961582baf1
Dmytro Belei <beleyhha@gmail.com>
2025-11-04 08:55:25 +0100

    feat: add note repository

---
+++ src/main/java/dev/dmbiee/securenote/features/note/NoteRepository.java
@@ -0,0 +1,11 @@
+package dev.dmbiee.securenote.features.note;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByOwner(String owner);
+}
